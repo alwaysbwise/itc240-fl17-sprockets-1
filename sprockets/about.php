db31d8fbf7fc8154e8b6c2783686707a8887d054
alwaysbwise <33883062+alwaysbwise@users.noreply.github.com>
2017-12-07 09:23:30 -0800

    new features added
    added upload image
    added widgets files
    added bitcoin widget

--- sprockets/about.php
+++ sprockets/about.php
@@ -3,25 +3,40 @@
 
 
         <hr class="divider">
-        <h2 class="text-center text-lg text-uppercase my-0">About
-          <strong>Business Casual</strong>
+        <h2 class="text-center text-lg text-uppercase my-0">Current Value: 
+          <strong>Bitcoin</strong>
         </h2>
         <hr class="divider">
         <div class="row">
           <div class="col-lg-6">
-            <img class="img-fluid mb-4 mb-lg-0" src="img/slide-2.jpg" alt="">
+           <p>You can donate Bitcoin (BTC) to me here:<br><br>
+              <b>1C4bHG91NFge6nMwF27xA6yCcBYz29TM7c</b>
+               <img src="images/POSTriskBTC2.png" alt="QR"  />
+              </p>
           </div>
           <div class="col-lg-6">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam soluta dolore voluptatem, deleniti dignissimos excepturi veritatis cum hic sunt perferendis ipsum perspiciatis nam officiis sequi atque enim ut! Velit, consectetur.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam pariatur perspiciatis reprehenderit illo et vitae iste provident debitis quos corporis saepe deserunt ad, officia, minima natus molestias assumenda nisi velit?</p>
+            
+ <div id="hitbtc-ticker" class="hit-big" data-hue="-171" data-refId="5a20abce5c4df"></div>
+<script type="text/javascript">
+    (function() {
+        var po = document.createElement("script");
+        po.type = "text/javascript";
+        po.async = true;
+        po.src = "https://hitbtc.com/get_widget?pair=btcusd";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+     // You can construct widget dynamically by calling hitbtc.widget("myDiv", "big", -171, "btcusd", "5a20abce5c4df");
+</script>
+
           </div>
         </div>
       </div>
 
       <div class="bg-faded p-4 my-4">
         <hr class="divider">
-        <h2 class="text-center text-lg text-uppercase my-0">Our
-          <strong>Team</strong>
+        <h2 class="text-center text-lg text-uppercase my-0">My Address<br>
+          <strong>1C4bHG91NFge6nMwF27xA6yCcBYz29TM7c</strong>
         </h2>
         <hr class="divider">
         <div class="row">
@@ -58,4 +73,4 @@
         </div>
 <?php 
 
-get_footer()
+get_footer();
---
+++ sprockets/includes/credentials.php
@@ -0,0 +1,7 @@
+<?php
+//credentials.php
+
+define('DB_NAME','sccxyz_db');
+define('DB_USER','brian_admin');
+define('DB_PASSWORD','+23zPF$o0ht');
+define('DB_HOST','mysql.brianwise.xyz');
